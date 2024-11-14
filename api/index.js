995e77fde20d858b342c294cf85c6d5ad9aa3f68
Sgissa <salmanegarba721@gmail.com>
2024-11-14 18:10:17 -0500

    first commit

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const friends = [];
+
+app.get("api/friends", function (req, res) {
+  res.status(200).json;
+  ({ friends: friends });
+});
+
+app.post("/api/friends/new", function (req, res) {
+  friends.push(req.body.myName);
+  res.status(200).json({
+    newFriendsTotal: friends.length,
+    friendAdded: req.body.myName,
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port);
+
+module.exports = app;
